ba02902d71b7b45d4993f2809858948ace378f7c
zurueck <zurueck@gmail.com>
2021-02-12 20:17:27 +0800

    add task part into sys
    Tag:
    Symptom:
    Description:
    Author: YM

--- rz_state_ctrl.c
+++ rz_state_ctrl.c
@@ -5,7 +5,8 @@
 //done
 static int is_jump_valid(struct rz_state *state, uint8_t des)
 {
-    printf("st = %s\n", state->name);
+
+    //printf("st = %s\n", state->name);
     if (state->jump_validList & (0x1 << des)) {
         return 1;   
     }
@@ -13,29 +14,28 @@ static int is_jump_valid(struct rz_state *state, uint8_t des)
     return 0;    
 }
 
-
 //done
-int state_ctrl_reg(struct rz_state_ctrl *ctrl, int size)
+int state_ctrl_regist(struct rz_state_ctrl *ctrl, int state_size, int task_size)
 {
     
     ctrl->prevState = 0;
     ctrl->nowState = 0;
     ctrl->nextState = 0;
     ctrl->state_num = size/sizeof(struct rz_state); 
+    ctrl->task_num = size/sizeof(struct rz_task); 
     
     return 0;
 }
 
-
 //done
 int is_StageChanging(struct rz_state_ctrl* ctrl)
 {
+
     if (ctrl->nowState != ctrl->nextState) {
         return 1;   
     }
     return 0;
 }
-
 
 //done
 int set_next_state(struct rz_state_ctrl* ctrl, int nextIdx)
@@ -63,16 +63,17 @@ void sw_state(struct rz_state_ctrl *ctrl)
     ctrl->nowState = ctrl->nextState;
 
     for (int m = 0; m < ctrl->state_num; m++) {
-        ctrl->state[m].swFlag = 0;
+        ctrl->task[m].sw_ready_flag = 0;
     }
 }
+
 
 //done
 int check_sw_flags(struct rz_state_ctrl *ctrl)
 {
 
-    for (int m = 0; m < ctrl->state_num; m++) {
-        if (!ctrl->state[m].swFlag) {
+    for (int m = 0; m < ctrl->task_num; m++) {
+        if (!ctrl->task[m].sw_ready_flag) {
             return -1;
         }
     }
@@ -80,11 +81,25 @@ int check_sw_flags(struct rz_state_ctrl *ctrl)
     return 0;
 }
 
+
 //done
 void print_state_info(struct rz_state_ctrl *ctrl)
 {
+
+    printf("State\n");
     for (int m = 0; m < ctrl->state_num; m++) {
         printf("    Index %d: %s\n", m, ctrl->state[m].name);
+    }
+}
+
+
+//done
+void print_task_info(struct rz_state_ctrl *ctrl)
+{
+
+    printf("Task\n");
+    for (int m = 0; m < ctrl->task_num; m++) {
+        printf("    Index %d: %s\n", m, ctrl->task[m].name);
     }
 }
 
--- rz_state_ctrl.h
+++ rz_state_ctrl.h
@@ -3,11 +3,18 @@
 
 #include <stdint.h>
 
+struct rz_task
+{
+    uint8_t index;
+    int8_t *name;
+    uint8_t sw_ready_flag;
+};
+
+
 struct rz_state
 {
     uint8_t index;
     int8_t *name;
-    uint8_t swFlag;
     uint32_t jump_validList;
 };
 
@@ -19,7 +26,9 @@ struct rz_state_ctrl
 
     uint8_t state_num;
     struct rz_state *state;
-    
+
+    uint8_t task_num;
+    struct rz_task *task;
 };
 
 #define __ADD_RZ_STATE(i, n, v) \
@@ -29,11 +38,19 @@ struct rz_state_ctrl
         .jump_validList = v, \
     }
 
-int state_ctrl_reg(struct rz_state_ctrl *ctrl, int size);
+#define __ADD_RZ_TASK(i, n) \
+    { \
+        .index = i, \
+        .name = n, \
+    }
+
+int state_ctrl_regist(struct rz_state_ctrl *ctrl, int size);
 int is_StageChanging(struct rz_state_ctrl* ctrl);
 int set_next_state(struct rz_state_ctrl* ctrl, int nextIdx);
 void sw_state(struct rz_state_ctrl *ctrl);
 int check_sw_flags(struct rz_state_ctrl *ctrl);
 void print_state_info(struct rz_state_ctrl *ctrl);
+void print_task_info(struct rz_state_ctrl *ctrl);
+struct rz_state *get_state_info(struct rz_state_ctrl *ctrl, uint8_t idx);
 
 #endif  /* RZ_STATE_CTRL */
